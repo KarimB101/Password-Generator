341750f1cec09235bce2e6a5d1f074c7ab87f1f3
KarimB101 <karim.baptiste@yahoo.com>
2022-12-22 23:45:16 -0600

    last changes to readme

--- script.js
+++ script.js
@@ -1,31 +1,126 @@
 // Assignment code here
-
-
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.',
+];
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+];
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+];
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
 
-  var lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
-  var uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  var numChars = '0123456789';
-  var specialChars = ',!@#$%^&*()_+-=';
+  
+  // Get password length with length
+var userOptions = {
 
-  var characters = ''
-  var password = '';
+}
 
-  var passwordLength = prompt("Please pick a number between 8 and 128");
 
-//if passwordLength does NOT meet the length requirements
-//rerun the function
 
-//else if passwordLength meets the length requirements
-//assk if they want to include certain characters in the password
+  //if passwordLength does NOT meet the length requirements
+  //rerun the function
+  var passwordLength = parseInt(prompt("Please pick a number between 8 and 128 for length of password"), 10);
 
+  if (Number.isNaN(passwordLength)) {
+    alert("Please provide a number")
+    passwordLength = null 
+    return
+  } else if (passwordLength < 8) {
+    alert("Please provide a number greater than 8")
+    passwordLength = null
+    return
+  } else if (passwordLength > 128) {
+    alert("Please provide a number less than 128")
+    passwordLength = null
+    return
+  } 
+    
+  //else if passwordLength meets the length requirements
+  //assk if they want to include certain characters in the password
+  // userChoice ("confirm", )
+  
   var lowercaseConfirm = confirm("Do you want lowercase in your password");
   var uppercaseConfirm = confirm("Do you want uppercase in your password");
   var numberConfirm = confirm("Do you want numbers in your password");
   var specialConfirm = confirm("Do you want special characters in your password");
+
 
   //if they want lowercase, then add lowercaseChars to characters
   //if they want uppercase, then add uppercaseChars to characters
